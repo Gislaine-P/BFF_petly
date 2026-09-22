f17f0df5099887e19f0da480411181885a2e887e
Thaidondetai <tha.caamano@duocuc.cl>
2026-09-22 00:27:18 -0300

    Cambios en el properties

--- src/services/marca.service.js
+++ src/services/marca.service.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 
-const MS_CATALOGO_URL = process.env.MS_CATALOGO_URL || 'http://127.0.0.1:8082/api/marca';
+const MS_MARCA_URL = process.env.MS_MARCA_URL || 'http://127.0.0.1:8082/api/marca';
 
 export const obtenerMarcas = async () => {
-  const response = await axios.get(MS_CATALOGO_URL);
+  const response = await axios.get(MS_MARCA_URL);
   return response.data;
 };
 
 export const crearMarca = async (nuevaMarca) => {
-  const response = await axios.post(MS_CATALOGO_URL, nuevaMarca);
+  const response = await axios.post(MS_MARCA_URL, nuevaMarca);
   return response.data;
 };
 
 export const actualizarMarca = async (id, marca) => {
-  const response = await axios.put(`${MS_CATALOGO_URL}/${id}`, marca);
+  const response = await axios.put(`${MS_MARCA_URL}/${id}`, marca);
   return response.data;
 };
 
 export const eliminarMarca = async (id) => {
-  const response = await axios.delete(`${MS_CATALOGO_URL}/${id}`);
+  const response = await axios.delete(`${MS_MARCA_URL}/${id}`);
   return response.data;
 };
